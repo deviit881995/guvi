a0ba516388ac14eb92f95c9a604d2d9f494457c0
deviit881995 <deviit881995@gmail.com>
2016-08-22 21:05:01 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,35 @@
+package d;
+
+import java.util.Scanner;
+
+public class positive {
+
+		public static void main(String args[])
+		
+		
+		{
+			Scanner sc=new Scanner(System.in);
+			
+			int n=sc.nextInt();
+			
+			int i;
+			
+			if(n>0)
+			{
+				
+				System.out.println("positive");
+			
+			}
+			else
+				
+			{
+				
+				System.out.println("negative");
+				
+			}
+			
+		}
+		
+	}
+	
+
